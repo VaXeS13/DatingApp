3cf53bae817159862195b0754645c4f685dbb768
mhinc <mhinc@K50394>
2021-03-22 13:53:21 +0100

    end

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -29,7 +29,7 @@ namespace API.Controllers
             _mapper = mapper;
             _userRepository = userRepository;
         }
-
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers([FromQuery]UserParams userParams)
         {
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { environment } from 'src/environments/environment';
+import { UserParams } from '../_models/userParams';
 import { User } from '../_models/users';
 @Injectable({
   providedIn: 'root'
@@ -37,6 +38,11 @@ export class AccountService {
   }
 
   setCurrentUser(user: User){
+    user.roles =[]
+    const roles = this.getDecodedToken(user.token).role
+
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles)
+    
     localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user)
   }
@@ -44,5 +50,9 @@ export class AccountService {
   logout(){
     localStorage.removeItem('user')
     this.currentUserSource.next(null)
+  }
+
+  getDecodedToken(token){
+    return JSON.parse(atob(token.split('.')[1]))
   }
 }
